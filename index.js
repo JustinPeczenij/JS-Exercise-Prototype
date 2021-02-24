b1ec05e7a13fe7c5bb377e8ec504eb1b02ce7a63
Justin Peczenij <justinpeczenij@gmail.com>
2021-02-24 18:01:09 -0500

    finished task three

--- index.js
+++ index.js
@@ -43,15 +43,11 @@ function Airplane(name) {
     this.name = name;
     this.age = age;
     this.stomach = [];
-    this.toString = function(){
-      return `${this.name}, ${this.age}`
-    }
   }
 
-
-  Person.prototype.eat = function(food){
+  Person.prototype.eat = function(edible){
     if(this.stomach.length < 10){
-    this.stomach.push(food);
+    this.stomach.push(edible);
     }
   }
 
@@ -59,6 +55,10 @@ function Airplane(name) {
     this.stomach = [];
   }
   
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`
+  }
+    
   
   /*
     TASK 2
@@ -92,11 +92,18 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+    this.name = name;
+    this.age = age;
+    this.favoriteToy = favoriteToy;
+}
+
+  Baby.prototype = Object.create(Person.prototype)
+
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`
   }
- 
-  
+
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
